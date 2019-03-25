0ef61ec753ce7fc6afd7ffacdb7292681c063d04
Merline Belizaire <merline@noukod.com>
2019-03-25 17:20:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
+
+function getToken() {
+  //change to your token to run in browser, but set
+  //back to '' before committing so all tests pass
+  return '';
+}
+
+
 function getIssues() {
+  fetch(`https://api.github.com/repos/Merlieb/javascript-fetch-lab/issues`,{
+    headers:{
+      Authorization:`${getToken()}`
+    }
+  }).then((response)=>response.json())
+  .then(showIssues)
 }
 
 function showIssues(json) {
 }
 
 function createIssue() {
-}
-
-function showResults(json) {
+  const issueData={
+    title:document.getElementById("title").value,
+    body:document.getElementById("body").value
+  }
+  fetch(`https://api.github.com/repos/Merlieb/javascript-fetch-lab/issues`,{
+    method:"post",
+    body:JSON.stringify(issueData),
+    headers:{
+      Authorization:`${getToken()}`
+    }
+  }).then((response)=>response.json())
+  .then(showIssues)
 }
 
 function forkRepo() {
+  const token=getToken();
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`,{
+    method:'post',
+    headers:{
+      Authorization:`${token}`
+    }
+  })
+  .then((response)=>response.json())
+  .then(showResults);
 }
 
-function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
-  return ''
+function showResults(jsonResponse){
+  const resultDiv=document.querySelector("#results");
+  resultDiv.innerHTML=jsonResponse.html_url;
 }
